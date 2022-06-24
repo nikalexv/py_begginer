6fff05fc2f438b86c5552cccef6feb028670dd45
nikalex <nikalex@vm6.template>
2022-06-24 14:07:49 +0300

    lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,32 @@
+def LevenshteinDistance(s, t):
+    n, m = len(t), len(s)
+    v0 = list(range(n))+[0]
+    v1 = [0]*(n + 1)
+
+    for i in range(m):
+        v1[0] = i + 1
+        #print(v1[0], end=' ')
+        for j in range(n):
+            deletionCost = v0[j+1] + 1
+            insertionCost = v1[j] + 1
+            if s[i] == t[i]:
+                substitutionCost = v0[j]
+            else:
+                substitutionCost = v0[j] + 1
+                
+            v1[j+1] = min(deletionCost, insertionCost, substitutionCost)
+
+        v0, v1 = v1, v0
+    return v0[n]
+
+pairs = [
+('море', 'гора'),
+('выбор', 'выборы'),
+('sunday', 'monday'),
+('роспись','подписи')
+]
+
+if __name__ == "__main__":
+    for s, t in pairs:
+        print(s,t, LevenshteinDistance(s, t))
+
